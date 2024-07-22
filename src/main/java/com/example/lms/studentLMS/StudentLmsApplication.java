f7644be944692894a5c6d712e66454e06738d54b
RajkumarRavadi <rajk142567@gmail.com>
2024-07-22 16:50:38 +0530

    code for Library Management System

---
+++ src/main/java/com/example/lms/studentLMS/StudentLmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.lms.studentLMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentLmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentLmsApplication.class, args);
+	}
+
+}

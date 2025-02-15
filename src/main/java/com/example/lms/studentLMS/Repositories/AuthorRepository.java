034d2f399441524d64b1cb0a944f0e7908851933
RajkumarRavadi <rajk142567@gmail.com>
2025-02-15 09:09:06 +0530

    upadte the autohor repo

--- src/main/java/com/example/lms/studentLMS/Repositories/AuthorRepository.java
+++ src/main/java/com/example/lms/studentLMS/Repositories/AuthorRepository.java
@@ -2,7 +2,10 @@ package com.example.lms.studentLMS.Repositories;
 
 import com.example.lms.studentLMS.Models.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Repository interface for Author entity.
@@ -10,5 +13,16 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
+
+    //here we are defining findAuthorByEmailId And FindAuthorByRatingsGraterThan methods
+    //no need to implement the methods they will get implemented automatically by the JPA Query
+    Author findAuthorByEmailId(String emailId);
+    List<Author> findAuthorsByRatingsGreaterThan(Double ratings);
     // No need to write implementation code; Spring Data JPA provides default methods.
+
+
+    //we can also use the native SQL queries if required
+    @Query(nativeQuery = true, value = "select * from author emailId =: emailId")
+    Author findAuthorInfo(String emailId);
 }
+
